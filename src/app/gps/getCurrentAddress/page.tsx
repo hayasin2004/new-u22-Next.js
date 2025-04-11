f45c6c87c2254daebda2c47876d9b6940c8fb123
hayasin2004 <masataka1kousuke1@gmail.com>
2025-04-12 08:12:04 +0900

    取得座標が曖昧だから正確ではない

--- src/app/gps/getCurrentAddress/page.tsx
+++ src/app/gps/getCurrentAddress/page.tsx
@@ -13,7 +13,6 @@ const GetCurrentAddress = () => {
             </div>
         )
 
-
 }
 
 
--- src/app/gps/testGps/page.tsx
+++ src/app/gps/testGps/page.tsx
@@ -12,73 +12,81 @@ import {
 import ControlPanel from "@/components/gps/control-panel/ControlPanel";
 import MovingMarker from "@/components/gps/moving-marker/MovingMarker";
 import MarkerWithInfowindow from "@/components/gps/marker-with-infowindow/MarkerWithInfowindow";
+import useGetCurrentAddress from "@/hooks/useGetCurrentAddress";
 
 const MapContent = () => {
+    const {location} = useGetCurrentAddress()
+    console.log(location.lng, location.lat)
     return (
-
         <APIProvider apiKey={process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY!}>
-            <Map defaultZoom={15} mapId={"307c4270c2518d3c"} style={{width:"1800px" , height:"1800px"}} defaultCenter={{ lat: 35.656, lng: 139.737 }} >
-                {/* simple marker */}
-                <Marker
-                    position={{lat: 10, lng: 10}}
-                    clickable={true}
-                    onClick={() => alert('marker was clicked!')}
-                    title={'clickable google.maps.Marker'}
-                />
+            {location ?
+                <>
+                    <Map defaultZoom={15} mapId={"307c4270c2518d3c"} style={{width: "1800px", height: "1800px"}}
+                         defaultCenter={{lat: location.lat!, lng: location.lng!}}>
+                        {/* simple marker */}
+                        <Marker
+                            position={{lat: 10, lng: 10}}
+                            clickable={true}
+                            onClick={() => alert('marker was clicked!')}
+                            title={'clickable google.maps.Marker'}
+                        />
 
-                {/* advanced marker with customized pin */}
-                <AdvancedMarker
-                    position={{lat: 20, lng: 10}}
-                    title={'AdvancedMarker with customized pin.'}>
-                    <Pin
-                        background={'#22ccff'}
-                        borderColor={'#1e89a1'}
-                        glyphColor={'#0f677a'}></Pin>
-                </AdvancedMarker>
 
-                {/* advanced marker with html pin glyph */}
-                <AdvancedMarker
-                    position={{lat: 15, lng: 20}}
-                    title={'AdvancedMarker with customized pin.'}>
-                    <Pin background={'#22ccff'} borderColor={'#1e89a1'} scale={1.4}>
-                        {/* children are rendered as 'glyph' of pin */}
-                        👀
-                    </Pin>
-                </AdvancedMarker>
+                        {/* advanced marker with customized pin */}
+                        <AdvancedMarker
+                            position={{lat: location.lat!, lng: location.lng!}}
+                            title={'AdvancedMarker with customized pin.'}>
+                            <Pin
+                                background={'#22ccff'}
+                                borderColor={'#1e89a1'}
+                                glyphColor={'#0f677a'}></Pin>
+                        </AdvancedMarker>
 
-                {/* advanced marker with html-content */}
-                <AdvancedMarker
-                    position={{lat: 30, lng: 10}}
-                    title={'AdvancedMarker with custom html content.'}>
-                    <div
-                        style={{
-                            width: 16,
-                            height: 16,
-                            position: 'absolute',
-                            top: 0,
-                            left: 0,
-                            background: '#1dbe80',
-                            border: '2px solid #0e6443',
-                            borderRadius: '50%',
-                            transform: 'translate(-50%, -50%)'
-                        }}></div>
-                </AdvancedMarker>
+                        {/* advanced marker with html pin glyph */}
+                        <AdvancedMarker
+                            position={{lat: 15, lng: 20}}
+                            title={'AdvancedMarker with customized pin.'}>
+                            <Pin background={'#22ccff'} borderColor={'#1e89a1'} scale={1.4}>
+                                {/* children are rendered as 'glyph' of pin */}
+                                👀
+                            </Pin>
+                        </AdvancedMarker>
 
-                {/* simple positioned infowindow */}
-                <InfoWindow position={{lat: 40, lng: 0}} maxWidth={200}>
-                    <p>
-                        This is the content for another infowindow with <em>HTML</em>
-                        -elements.
-                    </p>
-                </InfoWindow>
+                        {/* advanced marker with html-content */}
+                        <AdvancedMarker
+                            position={{lat: 30, lng: 10}}
+                            title={'AdvancedMarker with custom html content.'}>
+                            <div
+                                style={{
+                                    width: 16,
+                                    height: 16,
+                                    position: 'absolute',
+                                    top: 0,
+                                    left: 0,
+                                    background: '#1dbe80',
+                                    border: '2px solid #0e6443',
+                                    borderRadius: '50%',
+                                    transform: 'translate(-50%, -50%)'
+                                }}></div>
+                        </AdvancedMarker>
 
-                {/* continously updated marker */}
-                <MovingMarker />
+                        {/* simple positioned infowindow */}
+                        <InfoWindow position={{lat: 40, lng: 0}} maxWidth={200}>
+                            <p>
+                                This is the content for another infowindow with <em>HTML</em>
+                                -elements.
+                            </p>
+                        </InfoWindow>
 
-                {/* simple stateful infowindow */}
-                <MarkerWithInfowindow />
-            </Map>
+                        {/* continously updated marker */}
+                        <MovingMarker/>
 
+                        {/* simple stateful infowindow */}
+                        <MarkerWithInfowindow/>
+                    </Map>
+
+                </> : ""
+            }
         </APIProvider>
     )
 };

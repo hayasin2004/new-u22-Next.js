0d0917d05f52bb5b8acc9ed87628f7f7097488c0
hayasin2004 <masataka1kousuke1@gmail.com>
2025-04-12 13:27:31 +0900

    通知リンク訂正

--- public/sw.js
+++ public/sw.js
@@ -1,25 +1,25 @@
-
-self.addEventListener('push', function (event)  {
-    if (event.data){
+self.addEventListener('push', function (event) {
+    if (event.data) {
         const data = event.data.json()
         const options = {
             body: data.body,
-            icon : data.icon || "/images/PWA-Icon.png",
-            badge : "/images/PWA-Icon.png",
-            vibrate :[100 ,50 ,100],
-            data : {
-                dataOfArrival : Date.now(),
-                primaryKey : "2"
+            icon: data.icon || "/images/PWA-Icon.png",
+            badge: "/images/PWA-Icon.png",
+            vibrate: [100, 50, 100],
+            data: {
+                dataOfArrival: Date.now(),
+                primaryKey: "2"
             },
         }
-        event.waitUntil(self.registration.showNotification(data.title , options))
+        event.waitUntil(self.registration.showNotification(data.title, options))
     }
 })
 
-self.addEventListener('notificationclick', function (event)  {
+self.addEventListener('notificationclick', function (event) {
     console.log("通知をクリックして受け取りました")
     event.notification.close()
     event.waitUntil(clients.openWindow(
-        "http://localhost:3000/"
+        "http://localhost:3000/",
+        "https://new-u22-next-js-git-development-pwa-hayasin2004s-projects.vercel.app/"
     ))
 })

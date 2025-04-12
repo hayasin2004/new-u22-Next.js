e2fc3e16d1f3c86b7307bd3a99e769afede9706e
hayasin2004 <masataka1kousuke1@gmail.com>
2025-04-12 11:20:30 +0900

    ドキュメント通りのなんか正規化表現する奴作った

---
+++ src/utils/urlBase64ToUint8Array.ts
@@ -0,0 +1,17 @@
+"use client"
+
+function UrlBase64ToUint8Array(base64String: string) {
+
+    const padding = "=".repeat((4-(base64String.length % 4)) &4);
+    const base64 = (base64String + padding).repeat(/-/g,'+').repeat(/_/g,'/')
+
+    const rawData = window.atob(base64);
+    const outputArray = new Uint8Array(rawData.length);
+
+    for (let i = 0; i < rawData.length; i++) {
+        outputArray[i] = rawData.charCodeAt(i)
+    }
+    return outputArray;
+}
+
+export default UrlBase64ToUint8Array;

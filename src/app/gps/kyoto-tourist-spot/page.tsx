82cb8ff8b7d73e0ed6e0ea8c7944af535147fce2
hayasin2004 <masataka1kousuke1@gmail.com>
2025-04-12 10:00:15 +0900

    スマホサイズに対応、PWA用のパッケージインストール

---
+++ src/app/gps/kyoto-tourist-spot/page.tsx
@@ -0,0 +1,69 @@
+"use client"
+import React, {useEffect, useState} from 'react';
+import {kyotoTourist} from "@/dummyData/kyotoTourist";
+import {AdvancedMarker, APIProvider, InfoWindow, Map, useAdvancedMarkerRef} from "@vis.gl/react-google-maps";
+
+
+const KyotoTouristSpot = () => {
+    // ダミーデータにピンを指す用のやつ
+    const [markerRef] = useAdvancedMarkerRef();
+
+    // 端末に合わせて位置情報を合わせる物　→　カスタムフックであとから用意したい。
+    const [dimensions, setDimensions] = useState({width: 0, height: 0});
+
+    useEffect(() => {
+        // ここで端末ごとの幅高さを状態管理している
+        const handleResize = () => {
+            setDimensions({width: window.innerWidth, height: window.innerHeight});
+        }
+        handleResize()
+
+    //     ここで端末のウィンドウサイズ変更を監視
+        window.addEventListener("resize", handleResize);
+
+        return () => window.removeEventListener("resize", handleResize);
+
+    }, []);
+
+    const [selectedSpot, setSelectedSpot]
+        = useState<{ lat: number , lng: number ,description: string} | null>(null)
+    console.log(JSON.stringify(selectedSpot))
+    const kyotoTouristData = kyotoTourist
+
+    // dimensions →　寸法って意味
+    if (dimensions.width == 0 || dimensions.height == 0) {
+        return null;
+    }
+
+
+    return (
+        <APIProvider apiKey={process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY || ""}>
+            <Map defaultZoom={14}
+                 defaultCenter={{lat: 34.9946, lng: 135.7850}}
+                 mapId={"307c4270c2518d3c"} style={{width: `${dimensions.width}px` , height:`${dimensions.height}px`}}>
+                {kyotoTouristData.map((spot, index) => (
+                    <AdvancedMarker
+                        ref={markerRef}
+                        key={index}
+                        position={{lat: spot.lat, lng: spot.lng}}
+                        title={spot.name}
+                        clickable={true}
+                        onClick={() => setSelectedSpot({lat: spot.lat, lng: spot.lng ,description : spot.description})}
+                    />
+                ))}
+                {selectedSpot && selectedSpot.lat && selectedSpot.lng && (
+                    <InfoWindow
+                        position={{lat: selectedSpot.lat, lng: selectedSpot.lng}}
+                        maxWidth={200}
+                        onCloseClick={() => setSelectedSpot(null)}>
+                        {selectedSpot.description}
+                    </InfoWindow>
+                )}
+            </Map>
+
+        </APIProvider>
+    );
+}
+
+
+export default KyotoTouristSpot;

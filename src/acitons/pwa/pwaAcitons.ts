9228e03134c6bbd9fdddec8a97b833748147cd34
hayasin2004 <masataka1kousuke1@gmail.com>
2025-04-12 13:02:32 +0900

    メールアドレス訂正

--- src/acitons/pwa/pwaAcitons.ts
+++ src/acitons/pwa/pwaAcitons.ts
@@ -2,44 +2,42 @@
 import webpush, {PushSubscription} from 'web-push'
 
 webpush.setVapidDetails(
-    "masataka1kousuke1@gmail.com",
+    "mailto:masataka1kousuke1@gmail.com",
     process.env.NEXT_PUBLIC_VAPID_PUBLIC_KEY!,
     process.env.VAPID_PRIVATE_KEY!
 )
 
-let subscription : PushSubscription | null = null
+let subscription: PushSubscription | null = null
 
-export const subscribeUser =async (sub : PushSubscription) => {
+export const subscribeUser = async (sub: PushSubscription) => {
     subscription = sub
-    return {success : true}
+    return {success: true}
 }
 
-export const unsubscribeUser =async () => {
+export const unsubscribeUser = async () => {
     subscription = null
-    return {success : true}
+    return {success: true}
 }
 
 
-export const sendNotification = async (message :string) =>{
-    if (!subscription){
+export const sendNotification = async (message: string) => {
+    if (!subscription) {
         throw new Error("登録が完了していないです。")
     }
     try {
 
         await webpush.sendNotification(
-
             subscription,
             JSON.stringify({
                 title: "テスト通知",
-                body : message,
-                icon : "/images/PWA-Icon.png"
+                body: message,
+                icon: "/images/PWA-Icon.png"
             }),
-
         )
 
-    }catch (err){
+    } catch (err) {
         console.error(err)
-        return {success : false ,error : "通知の送信に失敗しました。"}
+        return {success: false, error: "通知の送信に失敗しました。"}
     }
 
 }

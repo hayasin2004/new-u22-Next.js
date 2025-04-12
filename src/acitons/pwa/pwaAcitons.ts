b6a237ac34a7305d53789ed70f5a11d362f9a9d6
hayasin2004 <masataka1kousuke1@gmail.com>
2025-04-12 13:04:56 +0900

    メールアドレス訂正,スマホで通知確認

--- src/acitons/pwa/pwaAcitons.ts
+++ src/acitons/pwa/pwaAcitons.ts
@@ -2,7 +2,7 @@
 import webpush, {PushSubscription} from 'web-push'
 
 webpush.setVapidDetails(
-    "mailto:masataka1kousuke1@gmail.com",
+    `mailto:${process.env.testEmail}`,
     process.env.NEXT_PUBLIC_VAPID_PUBLIC_KEY!,
     process.env.VAPID_PRIVATE_KEY!
 )
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import MapContent from "@/app/gps/testGps/page";
+import Notification from "@/app/pwa/notification/page";
 
 
 export default function Home() {
   return (
     <div >
-      <MapContent/>
+      {/*<MapContent/>*/}
+        <Notification/>
     </div>
   );
 }

9adba6efdcb96a39e566c06bf7c1ba117db1c0fe
hayasin2004 <masataka1kousuke1@gmail.com>
2025-04-12 10:10:06 +0900

    カスタムフックにリファクタリングしました

--- src/app/gps/getCurrentAddress/page.tsx
+++ src/app/gps/getCurrentAddress/page.tsx
@@ -7,7 +7,7 @@ const GetCurrentAddress = () => {
 
         return (
             <div>
-                <h1>現在地</h1>
+                <h1>現在地確認用ページ</h1>
                 <p>緯度: {location.lat}</p>
                 <p>経度: {location.lng}</p>
             </div>

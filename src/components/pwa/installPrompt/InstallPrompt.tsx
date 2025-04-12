8c6ed82465c3767434b85235f23d1939ac24e084
hayasin2004 <masataka1kousuke1@gmail.com>
2025-04-12 13:20:49 +0900

    型指定訂正

--- src/components/pwa/installPrompt/InstallPrompt.tsx
+++ src/components/pwa/installPrompt/InstallPrompt.tsx
@@ -1,13 +1,17 @@
 import React, {useEffect, useState} from 'react';
-
-const InstallPrompt = () => {
+declare global {
+    interface Window {
+        MSStream?: unknown; // `unknown` 型や適切な型を指定
+    }
+}
+const InstallPrompt :React.FC = () => {
     const [isIOS, setIsIOS] = useState(false)
     const [isStandalone, setIsStandalone] = useState(false)
 
     useEffect(() => {
         // IOSかどうかの判断
         setIsIOS(
-            /iPad|iPhone|iPod/.test(navigator.userAgent) && !(window as any).MSStream
+            /iPad|iPhone|iPod/.test(navigator.userAgent) && !window.MSStream
         )
         // ホーム画面にインストールされたかの状態の管理
         setIsStandalone(window.matchMedia('(display-mode: standalone)').matches)

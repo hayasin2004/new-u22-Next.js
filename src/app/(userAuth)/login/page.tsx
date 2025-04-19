c118884f44773fb978e8fce6ee04bfbf15cf6e24
Tacokunchan <takutaku041201@icloud,com>
2025-04-19 14:28:36 +0900

    一からNextAuth v5に変更

--- src/app/(userAuth)/login/page.tsx
+++ src/app/(userAuth)/login/page.tsx
@@ -1,7 +1,7 @@
 'use client'
 import React, {useState} from "react";
 import { useRouter } from "next/navigation";
-import {signIn} from "next-auth/react";
+// import { signIn } from "@/repository/prisma/NextAuthRepository";
 
  const LogIn = () => {
     const router = useRouter();
--- src/app/(userAuth)/signin/page.tsx
+++ src/app/(userAuth)/signin/page.tsx
@@ -1,7 +1,7 @@
 'use client'
 import React, {useState} from "react";
 import { useRouter } from "next/navigation";
-import { signIn } from "@/repository/prisma/NextAuthRepository";
+// import { signIn } from "@/repository/prisma/NextAuthRepository";
 
  const SignIn = () => {
     const router = useRouter();

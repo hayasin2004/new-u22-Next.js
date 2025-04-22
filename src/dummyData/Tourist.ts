35afe8915d368bea3e92e7ffb898daeee98a44cb
ikeda0322 <knt-ek322@au.com>
2025-04-22 10:54:06 +0900

    テストデータ更新

--- src/dummyData/Tourist.ts
+++ src/dummyData/Tourist.ts
@@ -4,4 +4,60 @@ export const Tourist = [
     {name: "嵐山", lat: 35.0094 , lng: 135.6781,description :"嵐山です",status : "京都府"},
     {name: "伏見稲荷大社", lat: 34.9671 , lng: 135.7727,description :"伏見稲荷大社です",status : "京都府"},
     {name: "銀閣寺", lat: 35.0272 , lng: 135.7985,description :"銀閣寺です",status : "京都府"},
+    // 北海道
+    {name: "清水寺", lat: 34.9946 , lng: 135.7850 ,description :"清水寺です" ,status : "北海道" },
+    //　東北
+    {name: "清水寺", lat: 34.9946 , lng: 135.7850 ,description :"清水寺です" ,status : "青森県" },
+    {name: "金閣寺", lat: 35.0394 , lng: 135.7292,description :"金閣寺です" ,status : "岩手県"},
+    {name: "嵐山", lat: 35.0094 , lng: 135.6781,description :"嵐山です",status : "宮城県"},
+    {name: "伏見稲荷大社", lat: 34.9671 , lng: 135.7727,description :"伏見稲荷大社です",status : "秋田県"},
+    {name: "銀閣寺", lat: 35.0272 , lng: 135.7985,description :"銀閣寺です",status : "山形県"},
+    {name: "銀閣寺", lat: 35.0272 , lng: 135.7985,description :"銀閣寺です",status : "福島県"},
+    // 関東
+    {name: "清水寺", lat: 34.9946 , lng: 135.7850 ,description :"清水寺です" ,status : "茨城県" },
+    {name: "金閣寺", lat: 35.0394 , lng: 135.7292,description :"金閣寺です" ,status : "栃木県"},
+    {name: "嵐山", lat: 35.0094 , lng: 135.6781,description :"嵐山です",status : "群馬県"},
+    {name: "伏見稲荷大社", lat: 34.9671 , lng: 135.7727,description :"伏見稲荷大社です",status : "埼玉県"},
+    {name: "銀閣寺", lat: 35.0272 , lng: 135.7985,description :"銀閣寺です",status : "千葉県"},
+    {name: "銀閣寺", lat: 35.0272 , lng: 135.7985,description :"銀閣寺です",status : "東京都"},
+    {name: "銀閣寺", lat: 35.0272 , lng: 135.7985,description :"銀閣寺です",status : "神奈川県"},
+    //　中部
+    {name: "清水寺", lat: 34.9946 , lng: 135.7850 ,description :"清水寺です" ,status : "新潟県" },
+    {name: "金閣寺", lat: 35.0394 , lng: 135.7292,description :"金閣寺です" ,status : "富山県"},
+    {name: "嵐山", lat: 35.0094 , lng: 135.6781,description :"嵐山です",status : "石川県"},
+    {name: "伏見稲荷大社", lat: 34.9671 , lng: 135.7727,description :"伏見稲荷大社です",status : "福井県"},
+    {name: "銀閣寺", lat: 35.0272 , lng: 135.7985,description :"銀閣寺です",status : "山梨県"},
+    {name: "銀閣寺", lat: 35.0272 , lng: 135.7985,description :"銀閣寺です",status : "長野県"},
+    {name: "銀閣寺", lat: 35.0272 , lng: 135.7985,description :"銀閣寺です",status : "岐阜県"},
+    {name: "銀閣寺", lat: 35.0272 , lng: 135.7985,description :"銀閣寺です",status : "静岡県"},
+    {name: "銀閣寺", lat: 35.0272 , lng: 135.7985,description :"銀閣寺です",status : "愛知県"},
+    // 近畿
+    {name: "清水寺", lat: 34.9946 , lng: 135.7850 ,description :"清水寺です" ,status : "三重県" },
+    {name: "金閣寺", lat: 35.0394 , lng: 135.7292,description :"金閣寺です" ,status : "滋賀県"},
+    {name: "嵐山", lat: 35.0094 , lng: 135.6781,description :"嵐山です",status : "大阪府"},
+    {name: "伏見稲荷大社", lat: 34.9671 , lng: 135.7727,description :"伏見稲荷大社です",status : "兵庫県"},
+    {name: "銀閣寺", lat: 35.0272 , lng: 135.7985,description :"銀閣寺です",status : "奈良県"},
+    {name: "銀閣寺", lat: 35.0272 , lng: 135.7985,description :"銀閣寺です",status : "和歌山県"},
+    //　中国
+    {name: "清水寺", lat: 34.9946 , lng: 135.7850 ,description :"清水寺です" ,status : "鳥取県" },
+    {name: "金閣寺", lat: 35.0394 , lng: 135.7292,description :"金閣寺です" ,status : "島根県"},
+    {name: "嵐山", lat: 35.0094 , lng: 135.6781,description :"嵐山です",status : "岡山県"},
+    {name: "伏見稲荷大社", lat: 34.9671 , lng: 135.7727,description :"伏見稲荷大社です",status : "広島県"},
+    {name: "銀閣寺", lat: 35.0272 , lng: 135.7985,description :"銀閣寺です",status : "山口県"},
+    // 四国
+    {name: "銀閣寺", lat: 35.0272 , lng: 135.7985,description :"銀閣寺です",status : "徳島県"},
+    {name: "銀閣寺", lat: 35.0272 , lng: 135.7985,description :"銀閣寺です",status : "香川県"},
+    {name: "銀閣寺", lat: 35.0272 , lng: 135.7985,description :"銀閣寺です",status : "愛媛県"},
+    {name: "銀閣寺", lat: 35.0272 , lng: 135.7985,description :"銀閣寺です",status : "高知県"},
+    //　九州
+    {name: "清水寺", lat: 34.9946 , lng: 135.7850 ,description :"清水寺です" ,status : "福岡県" },
+    {name: "金閣寺", lat: 35.0394 , lng: 135.7292,description :"金閣寺です" ,status : "佐賀県"},
+    {name: "嵐山", lat: 35.0094 , lng: 135.6781,description :"嵐山です",status : "長崎県"},
+    {name: "伏見稲荷大社", lat: 34.9671 , lng: 135.7727,description :"伏見稲荷大社です",status : "熊本県"},
+    {name: "銀閣寺", lat: 35.0272 , lng: 135.7985,description :"銀閣寺です",status : "大分県"},
+    {name: "銀閣寺", lat: 35.0272 , lng: 135.7985,description :"銀閣寺です",status : "宮崎県"},
+    {name: "銀閣寺", lat: 35.0272 , lng: 135.7985,description :"銀閣寺です",status : "鹿児島県"},
+    {name: "銀閣寺", lat: 35.0272 , lng: 135.7985,description :"銀閣寺です",status : "沖縄県"},
+
+
 ]

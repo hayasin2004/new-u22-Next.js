e4f05a6224fc2b8428f588ec0f5e57c498d4d8c5
Tacokunchan <takutaku041201@icloud,com>
2025-05-13 09:28:48 +0900

    一からNextAuth v5に変更

--- src/app/(userAuth)/login/page.tsx
+++ src/app/(userAuth)/login/page.tsx
@@ -1,51 +1,22 @@
-'use client'
-import React, {useState} from "react";
-import { useRouter } from "next/navigation";
-// import { signIn } from "@/repository/prisma/NextAuthRepository";
+import React from 'react';
 
- const LogIn = () => {
-    const router = useRouter();
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
+const Login = () => {
+    const [email, setEmail] = React.useState('');
+    const [password, setPassword] = React.useState('');
 
-
-    const handleLogin = async (e: React.MouseEvent<HTMLElement, MouseEvent>) =>{
-        e.preventDefault();
-        // ここでNextAuth認証が行われる
-        const result = await signIn("credentials", {
-            email,
-            password,
-            redirect: false,
-        });
-        console.log(result);
-        if(result?.error){
-            window.alert(result?.error);
-        }else{
-            router.push('/Home');
-        }
-
+    const handleLoginAction = () => {
+        setEmail('');
     }
-    const handleChange = () => {
-        router.push('/signin');
-    }
-
 
     return (
         <div>
-            <form>
-            <label>email</label>
-            <input
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}/>
-            <label>password</label>
-            <input
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}/>
-            <button onClick={handleLogin}>ログイン</button>
-            </form>
-            <button onClick={handleChange}>新規登録へ</button>
+            <label>Email:</label>
+            <input value={email} onChange={(e) => setEmail(e.target.value)} />
+            <label>Password:</label>
+            <input value={password} onChange={(e) => setPassword(e.target.value)} />
+            <button onClick={handleLoginAction}>Login</button>
         </div>
     );
 };
 
-export default LogIn;
+export default Login;
--- src/app/(userAuth)/signin/page.tsx
+++ src/app/(userAuth)/signin/page.tsx
@@ -1,44 +1,21 @@
-'use client'
-import React, {useState} from "react";
-import { useRouter } from "next/navigation";
-// import { signIn } from "@/repository/prisma/NextAuthRepository";
+import React from 'react';
 
- const SignIn = () => {
-    const router = useRouter();
-    const [username, setUsername] =useState<string>("");
-    const [email, setEmail] = useState<string>("");
-    const [password, setPassword] = useState<string>("");
-    const handleLogin = async (e: React.MouseEvent<HTMLElement, MouseEvent>) =>{
-        e.preventDefault();
-        const response = await signIn(email, username, password);
-        console.log(response);
-        router.push("/login");
-
+const SignIn = () => {
+    const [email, setEmail] = React.useState('');
+    const[username, setUsername] = React.useState('');
+    const [password, setPassword] = React.useState('');
+    const handleSignInAction = async () => {
+        setEmail('');
     }
-    const handleChange = () => {
-        router.push('/login');
-    }
-
-
     return (
         <div>
-            <form>
-            <label>name</label>
-            <input
-                value={username}
-                onChange={(e) => setUsername(e.target.value)}/>
-            <label>email</label>
-            <input
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}/>
-            <label>password</label>
-            <input
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}/>
-            <button onClick={handleLogin}>新規登録</button>
-            </form>
-            <button　onClick={handleChange}>ログインはこちら</button>
-
+            <label>email:</label>
+            <input type="text" value={email} onChange={(e) => setEmail(e.target.value)} />
+            <label>username:</label>
+            <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} />
+            <label>password:</label>
+            <input type="text" value={password} onChange={(e) => setPassword(e.target.value)} />
+            <button onClick={handleSignInAction}>SignIn</button>
         </div>
     );
 };

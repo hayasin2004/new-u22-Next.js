49921fce9f1a876e90555eb2676311264dfe8115
hrt05 <hirate05program@gmail.com>
2025-05-27 10:06:36 +0900

    Imageコンポーネントを使用して、Kyoto loadingコンポーネント内の画像を最適化しました。幅と高さを指定し、コードの可読性を向上させました。

--- src/components/(load)/kyotoLoad/kyotoLoading.tsx
+++ src/components/(load)/kyotoLoad/kyotoLoading.tsx
@@ -1,12 +1,13 @@
 import React from "react";
 import classes from "./style.module.css";
+import Image from "next/image";
 
 const KytLoad: React.FC = () => (
   <div className={classes.box}>
     <div className={classes.group}>
       <div className={classes["overlap-group"]}>
         <div className={classes.overlap}>
-        <div className={classes["text-wrapper"]}>TIME TO TRAVEL</div>
+          <div className={classes["text-wrapper"]}>TIME TO TRAVEL</div>
           <div className={classes.div}>KYOTO - JAPAN</div>
         </div>
         <div className={classes["JAPAN-TRAVEL-KEY"]}>
@@ -17,7 +18,13 @@ const KytLoad: React.FC = () => (
         <div className={classes["div-wrapper"]}>
           <div className={classes["text-wrapper-2"]}>SHOW AROUND</div>
         </div>
-        <img className={classes.HEX} alt="Kyoto travel background" src="/images/ScrollKyoto.png" />
+        <Image
+          className={classes.HEX}
+          alt="Kyoto travel background"
+          src="/images/ScrollKyoto.png"
+          width={88} //適切な幅を指定
+          height={88} //適切な高さを指定
+        />
       </div>
     </div>
   </div>
